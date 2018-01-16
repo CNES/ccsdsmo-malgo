0cdb8dbb5442150c92a27ebf44deef204178bf19
freyssin <andre.freyssinet@scalagent.com>
2018-01-16 11:26:10 +0100

    Interface changes needed to implement SplitBinary encoding.

--- src/mal/encoding.go
+++ src/mal/encoding.go
@@ -26,17 +26,24 @@ package mal
 import ()
 
 type Buffer interface {
-	Buffer() []byte
 	Write(v byte) error
 	Write16(v uint16) error
 	Write32(v uint32) error
 	Write64(v uint64) error
 	WriteBytes(v []byte) error
-	Reset()
+	// Writes an unsigned varint as defined in 5.25 section of the specification.
+	WriteUVarInt(value uint64) error
+	WriteFlag(f bool) error
+	// Reset the buffer allowing to reuse it
+	Reset(write bool)
 	Read() (byte, error)
 	Read16() (uint16, error)
 	Read32() (uint32, error)
 	Read64() (uint64, error)
 	ReadBytes(buf []byte) error
+	// Reads an unsigned varint as defined in 5.25 section of the specification.
+	ReadUVarInt() (uint64, error)
+	ReadFlag() (bool, error)
+	// Returns the part of buffer that still needs to be decoded
 	Remaining() ([]byte, error)
 }
