67b8ee851eb435314e6487aa91d37d4c6c217236
freyssin <andre.freyssinet@scalagent.com>
2018-01-19 09:00:57 +0100

    Adds a logging wrapper (actually use loggo).

---
+++ src/mal/debug/logger.go
@@ -0,0 +1,65 @@
+package debug
+
+import (
+	log "github.com/juju/loggo"
+)
+
+func init() {
+	Init("<root>=ERROR;mal=WARNING")
+}
+
+type Logger interface {
+	IsDebugEnabled() bool
+	IsInfoEnabled() bool
+	IsWarnEnabled() bool
+	Fatalf(message string, args ...interface{})
+	Errorf(message string, args ...interface{})
+	Warnf(message string, args ...interface{})
+	Infof(message string, args ...interface{})
+	Debugf(message string, args ...interface{})
+}
+
+func Init(args string) {
+	log.ConfigureLoggers(args)
+}
+
+func GetLogger(name string) Logger {
+	logger := log.GetLogger(name)
+	return &LoggerX{logger: logger}
+}
+
+type LoggerX struct {
+	logger log.Logger
+}
+
+func (l *LoggerX) IsDebugEnabled() bool {
+	return l.logger.IsDebugEnabled()
+}
+
+func (l *LoggerX) IsInfoEnabled() bool {
+	return l.logger.IsInfoEnabled()
+}
+
+func (l *LoggerX) IsWarnEnabled() bool {
+	return l.logger.IsWarningEnabled()
+}
+
+func (l *LoggerX) Fatalf(message string, args ...interface{}) {
+	l.logger.Criticalf(message, args...)
+}
+
+func (l *LoggerX) Errorf(message string, args ...interface{}) {
+	l.logger.Errorf(message, args...)
+}
+
+func (l *LoggerX) Warnf(message string, args ...interface{}) {
+	l.logger.Warningf(message, args...)
+}
+
+func (l *LoggerX) Infof(message string, args ...interface{}) {
+	l.logger.Infof(message, args...)
+}
+
+func (l *LoggerX) Debugf(message string, args ...interface{}) {
+	l.logger.Debugf(message, args...)
+}
