536ed7b62630ae2fe9ca6a26c53618ccc085219e
freyssin <andre.freyssinet@scalagent.com>
2019-02-08 15:47:44 +0100

    Use of generic Body and message API allowing the parameter encoding.

--- malactor/local_test.go
+++ malactor/local_test.go
@@ -1,3 +1,26 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2018 - 2019 ccsdsmo
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package malactor_test
 
 import (
@@ -37,22 +60,26 @@ func TestLocal(t *testing.T) {
 	fmt.Println("consumer: ", endpoint, err)
 
 	time.Sleep(1 * time.Second)
+	body := ctx.NewBody()
+	body.EncodeLastParameter(NewString("message1"), false)
 	msg1 := &Message{
 		UriFrom: consumer.Uri,
 		UriTo:   actor.Uri(),
-		Body:    []byte("message1"),
+		Body:    body,
 	}
 	consumer.Send(msg1)
-	fmt.Println("Message sent: ", string(msg1.Body))
+	fmt.Println("Message sent: ", msg1.Body)
 
 	time.Sleep(250 * time.Millisecond)
+	body = ctx.NewBody()
+	body.EncodeLastParameter(NewString("message2"), false)
 	msg2 := &Message{
 		UriFrom: consumer.Uri,
 		UriTo:   actor.Uri(),
-		Body:    []byte("message2"),
+		Body:    body,
 	}
 	consumer.Send(msg2)
-	fmt.Println("Message sent: ", string(msg2.Body))
+	fmt.Println("Message sent: ", msg2.Body)
 
 	time.Sleep(250 * time.Millisecond)
 	actor.Stop()
